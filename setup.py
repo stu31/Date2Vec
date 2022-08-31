c0c75c263913abafd7bf5ae61a5ad22c7c4c83b8
stu31 <46904862+stu31@users.noreply.github.com>
2022-08-31 16:18:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='Date2Vec',
       author='Surya Kant Sahu',
       author_email='surya.oju@gmail.com',
       url='https://github.com/ojus1/Date2Vec',
-      packages=['distutils', 'distutils.command'],
+      packages=['Date2Vec'],
       package_dir = {'': '.'}
      )
