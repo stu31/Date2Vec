5cdbadaad21556e5f1f830bcbdf1d8d51378293e
stu31 <46904862+stu31@users.noreply.github.com>
2022-08-31 16:15:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(name='Date2Vec',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='Pretrained models and scripts to train new models to get embeddings of Time-Date data',
       author='Surya Kant Sahu',
       author_email='surya.oju@gmail.com',
       url='https://github.com/ojus1/Date2Vec',
       packages=['distutils', 'distutils.command'],
+      package_dir = {'': '.'}
      )
