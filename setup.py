967cfeaa7e240f3100260d9e2a78706df861cc77
stu31 <46904862+stu31@users.noreply.github.com>
2022-08-31 16:11:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Date2Vec',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Surya Kant Sahu',
+      author_email='surya.oju@gmail.com',
+      url='https://github.com/ojus1/Date2Vec',
+      packages=['distutils', 'distutils.command'],
+     )
